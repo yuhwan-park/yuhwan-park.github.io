fdb5d33ded3da810d561fe576c92371278a95cd2
yuhwan-park <apkdkvkz@gmail.com>
2024-10-23 21:12:20 +0900

    styles: configure Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,19 @@
 import './globals.css';
+import { Nanum_Myeongjo } from 'next/font/google';
+import localFont from 'next/font/local';
+
+const nanum = Nanum_Myeongjo({
+  subsets: ['latin'],
+  display: 'swap',
+  style: 'normal',
+  variable: '--font-sans',
+  weight: ['400', '700', '800'],
+});
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-mono',
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html className={`${nanum.variable} ${pretendard.variable}`}>
       <body>{children}</body>
     </html>
   );
