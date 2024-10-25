5824605cdd54d009cb922f244806ea5c921f8b4d
viva-jake <jake@adoc.co.kr>
2024-10-25 18:33:56 +0900

    [FEAT] : Add parseToC util

--- libs/post.ts
+++ libs/post.ts
@@ -1,4 +1,5 @@
 import { allPosts } from 'contentlayer/generated';
+
 import { Post, ReducedPost } from './types';
 
 const reducePost = ({ body: _, _raw, _id, ...post }: Post): ReducedPost => post;
@@ -11,3 +12,45 @@ export const allBlogPosts: Post[] = allPosts
   .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 export const reducedAllBlogPosts = allBlogPosts.map(reducePost);
+
+export type TOCSection = TOCSubSection & {
+  subSections: TOCSubSection[];
+};
+
+export type TOCSubSection = {
+  slug: string;
+  text: string;
+};
+
+export const parseToc = (source: string) => {
+  return source
+    .split('\n')
+    .filter((line) => line.match(/(^#{1,3})\s/))
+    .reduce<TOCSection[]>((ac, rawHeading) => {
+      const nac = [...ac];
+      const removeMdx = rawHeading
+        .replace(/^##*\s/, '')
+        .replace(/[\*,\~]{2,}/g, '')
+        .replace(/(?<=\])\((.*?)\)/g, '')
+        .replace(/(?<!\S)((http)(s?):\/\/|www\.).+?(?=\s)/g, '');
+
+      const section = {
+        slug: removeMdx
+          .trim()
+          .toLowerCase()
+          .replace(/[^a-z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣 -]/g, '')
+          .replace(/\s/g, '-'),
+        text: removeMdx,
+      };
+
+      const isSubTitle = rawHeading.split('#').length - 1 === 3;
+
+      if (ac.length && isSubTitle) {
+        nac.at(-1)?.subSections.push(section);
+      } else {
+        nac.push({ ...section, subSections: [] });
+      }
+
+      return nac;
+    }, []);
+};
