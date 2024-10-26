1b47bf7ef02ac6f088074337ec68dff564ad63db
viva-jake <jake@adoc.co.kr>
2024-10-26 17:56:41 +0900

    refactor: remove console.log

--- components/TableOfContent.tsx
+++ components/TableOfContent.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { useEffect } from 'react';
-
 import { $ } from '@/libs/core';
 import { TOCSection } from '@/libs/post';
 import useTocScroll from '@/libs/useTocScroll';
@@ -13,51 +11,48 @@ const TOCItem = ({
   section: TOCSection;
   currentSectionSlug?: string;
   level?: number;
-}) => {
-  console.log('first', section.text, level);
-  return (
-    <>
-      <li
-        className={$(
-          'flex',
-          level === 1 && 'pl-3 text-[13px]',
-          level === 2 && 'pl-6 text-xs',
-          currentSectionSlug === section.slug && 'font-semibold',
-        )}
-      >
-        <a
-          className={$(
-            'link text-second',
-            'transition-all duration-200',
-            currentSectionSlug === section.slug &&
-              `${
-                level === 0
-                  ? 'text-[15px]'
-                  : level === 1
-                    ? 'text-[14px]'
-                    : level === 2 && 'text-[13px]'
-              }`,
-          )}
-          href={`#${section.slug}`}
-        >
-          {section.text}
-        </a>
-      </li>
-      {section.subSections.length > 0 && (
-        <ul className="mt-1.5 space-y-1.5">
-          {section.subSections.map((subSection, j) => (
-            <TOCItem
-              key={j}
-              section={subSection}
-              currentSectionSlug={currentSectionSlug}
-              level={level + 1}
-            />
-          ))}
-        </ul>
+}) => (
+  <>
+    <li
+      className={$(
+        'flex',
+        level === 1 && 'pl-3 text-[13px]',
+        level === 2 && 'pl-6 text-xs',
+        currentSectionSlug === section.slug && 'font-semibold',
       )}
-    </>
-  );
-};
+    >
+      <a
+        className={$(
+          'link text-second',
+          'transition-all duration-200',
+          currentSectionSlug === section.slug &&
+            `${
+              level === 0
+                ? 'text-[15px]'
+                : level === 1
+                  ? 'text-[14px]'
+                  : level === 2 && 'text-[13px]'
+            }`,
+        )}
+        href={`#${section.slug}`}
+      >
+        {section.text}
+      </a>
+    </li>
+    {section.subSections.length > 0 && (
+      <ul className="mt-1.5 space-y-1.5">
+        {section.subSections.map((subSection, j) => (
+          <TOCItem
+            key={j}
+            section={subSection}
+            currentSectionSlug={currentSectionSlug}
+            level={level + 1}
+          />
+        ))}
+      </ul>
+    )}
+  </>
+);
 
 export default function TableOfContent({
   toc,
@@ -68,10 +63,6 @@ export default function TableOfContent({
   className?: string;
 }) {
   const { currentSectionSlug } = useTocScroll(toc);
-
-  useEffect(() => {
-    console.log('currentSectionSlug', currentSectionSlug);
-  }, [currentSectionSlug]);
 
   return (
     <aside
