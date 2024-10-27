f048a5f41dde416460b45c2b784f57bc7868e71e
viva-jake <jake@adoc.co.kr>
2024-10-27 17:01:22 +0900

    remove: remove next-sitemap config

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-  siteUrl: 'https://yuhwan-park.github.io/',
-  changefreq: 'daily',
-  priority: 1,
-  sitemapSize: 5000,
-  generateRobotsTxt: true,
-};
